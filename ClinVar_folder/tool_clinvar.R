fd827c2dfaa6558619c7cc1f914d8410da0791d2
EmilyWang1106 <u7227972@anu.edu.au>
2023-10-27 16:14:30 +1100

    Create tool_clinvar.R
    This is the code to join the ddg value to the master table. This step was done on the virtual machine

---
+++ ClinVar_folder/tool_clinvar.R
@@ -0,0 +1,28 @@
+#!/usr/bin/Rscript
+
+#The file has been created to incorporate the DDG values for the 20 proteins in using teh database of stability_sort. 
+#The following steps have been done on the virtual machine
+suppressPackageStartupMessages({
+  library(data.table)
+  library(duckdb)
+  library(logger)
+})
+args <- commandArgs(trailingOnly = TRUE)
+log_threshold(DEBUG) #change DEBUG to INFO if you don't want the debug lines 
+master.table<-fread("Master_table.csv")
+con<-dbConnect(duckdb::duckdb(), dbdir=":memory:")
+res0a<-dbExecute(con,paste0("pragma temp_directory='/mnt/tmp';"))
+res0b<-dbExecute(con,"pragma memory_limit='30GB';")
+res0c<-dbExecute(con,"pragma threads=8;")
+duckdb::duckdb_register(con,"Master_table",master.table)
+res2<-dbExecute(con,"create or replace view asg as select uniprot, aa_pos, aa_alt, ddg FROM parquet_scan('/mnt/StabilitySort/AM_SS_G312.parquet') where fold='F1';")
+log_debug('asg: {dbGetQuery(con,"select count(*) FROM asg;")}')
+log_debug('master: {dbGetQuery(con,"select count(*) FROM master_table;")}')
+res3<-dbExecute(con,"create or replace view master_joined as select distinct * FROM master_table left join asg using (uniprot,aa_pos,aa_alt);")
+log_debug('master_joined: {dbGetQuery(con,"select count(*) FROM master_joined;")}')
+res4<-dbExecute(con,"copy master_joined to 'master_joined.csv'(format csv,header);")
+dbDisconnect(con,shutdown=TRUE)
+
+
+
+
